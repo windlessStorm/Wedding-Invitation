14f5534a3d411cd6f8cbf64e39e394bd7784a3f1
divya04das <53930535+divya04das@users.noreply.github.com>
2022-06-02 14:53:30 +0530

    efaefa

--- js/script.js
+++ js/script.js
@@ -93,7 +93,7 @@ var styles2 = [
     , 'font-size: 32px'
 ].join(';');
 
-console.log('\n\n%c SAVE THE DATE: 29th Nov, 2020!', styles);
+console.log('\n\n%c SAVE THE DATE: 5th July, 2022!', styles);
 
 console.log('%cYour presence is requested!%c\n\nRegards: Divya Das', styles1, styles2);
 
